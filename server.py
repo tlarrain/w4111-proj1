dd738ee4e37c2cacfe14f98cfa655554443edb91
rahimai <lawanrahim.ai@gmail.com>
2019-11-08 18:38:56 -0500

    test

--- server.py
+++ server.py
@@ -29,7 +29,7 @@ app = Flask(__name__, template_folder=tmpl_dir)
 #
 #     DATABASEURI = "postgresql://gravano:foobar@35.243.220.243/proj1part2"
 #
-DATABASEURI = "postgresql://user:password@35.243.220.243/proj1part2"
+DATABASEURI = "postgresql://tal2150:7764@35.243.220.243/proj1part2"
 
 
 #
